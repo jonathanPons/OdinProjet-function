a28db70c66225c05bef43dcf8ad4d0c9fa432667
Joponspolo <159774189+jonathanPons@users.noreply.github.com>
2025-12-03 07:07:12 +0100

    rockPapersScissors

---
+++ RockPaperScissors.js
@@ -0,0 +1,19 @@
+//Write the logic to get the computer choice
+
+//1.Create a new function named getComputerChoice.
+
+function getComputerChoice(params) {}
+//Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
+//Create a new function named getHumanChoice.
+function getHumanChoice(params) {}
+
+//Declare the players score variables
+
+//Create two new variables named humanScore and computerScore in the global scope.
+
+let humanScore = 0;
+let computerScore = 0;
+
+//Create a new function named playRound.
+
+function playRound(humanChoice, computerChoice) {}
