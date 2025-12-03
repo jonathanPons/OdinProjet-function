a4b71e9d2720fc92e0a07dfc6af9ff24830ad31b
Joponspolo <159774189+jonathanPons@users.noreply.github.com>
2025-12-03 08:00:34 +0100

    get computer choice OK

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,11 +1,22 @@
 //Write the logic to get the computer choice
 
 //1.Create a new function named getComputerChoice.
-
-function getComputerChoice(params) {}
 //Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
+function getComputerChoice() {
+  const array = ["Rock", "Paper", "Scissors"];
+
+  const numberRamdom = Math.floor(Math.random() * 3);
+  return array[numberRamdom];
+}
+console.log(getComputerChoice());
+
+//Write the logic to get the human choice
 //Create a new function named getHumanChoice.
-function getHumanChoice(params) {}
+function getHumanChoice(params) {
+  const choix = prompt("Quel est ton prénom ?");
+  console.log(choix); // Pour voir ce que l'utilisateur a tapé
+  return choix;
+}
 
 //Declare the players score variables
 
