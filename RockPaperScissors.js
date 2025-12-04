c876db6462c2a942af5c0dfdb104f6a22461d194
Joponspolo <159774189+jonathanPons@users.noreply.github.com>
2025-12-04 07:59:50 +0100

    suivi jeu RockPaperScissors

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -12,11 +12,15 @@ console.log(getComputerChoice());
 
 //Write the logic to get the human choice
 //Create a new function named getHumanChoice.
-function getHumanChoice(params) {
-  const choix = prompt("Quel est ton prénom ?");
-  console.log(choix); // Pour voir ce que l'utilisateur a tapé
-  return choix;
+let reponseUser = "";
+console.log("La fonction est appelée");
+
+function getHumanChoice() {
+  const reponsePrompt = prompt("Rock  Paper  Scissors");
+  return reponsePrompt;
 }
+const resultat = getHumanChoice();
+console.log("Tu as choisi :" + resultat);
 
 //Declare the players score variables
 
@@ -27,4 +31,34 @@ let computerScore = 0;
 
 //Create a new function named playRound.
 
-function playRound(humanChoice, computerChoice) {}
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("Égalité ! Vous avez tous les deux choisi " + humanChoice);
+  } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+    humanScore = humanScore + 1;
+    console.log("Tu gagnes, Rock bat Scissors");
+  } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+    humanScore = humanScore + 1;
+    console.log("Tu gagnes, Paper bat Rock");
+  } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+    humanScore = humanScore + 1;
+    console.log("Tu gagnes, Scissors bat Paper");
+  } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+    computerScore = computerScore + 1;
+    console.log("L'ordinateur gagne, Rock bat Scissors");
+  } else if (humanChoice === "Rock" && computerChoice === "Paper") {
+    computerScore = computerScore + 1;
+    console.log("L'ordinateur gagne, Paper bat Rock");
+  } else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+    computerScore = computerScore + 1;
+    console.log("L'ordinateur gagne, Scissors bat Paper");
+  }
+}
+playRound("Rock", "Paper");
+console.log(
+  "Score avant : Humain = " + humanScore + ", Ordinateur = " + computerScore
+);
+playRound("Rock", "Paper"); // L'ordinateur devrait gagner
+console.log(
+  "Score après : Humain = " + humanScore + ", Ordinateur = " + computerScore
+);
