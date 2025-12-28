c0106e54aff234b04cd9a745f99a45c3b6f5cd87
Joponspolo <159774189+jonathanPons@users.noreply.github.com>
2025-12-28 14:35:34 +0100

    new area

--- FoundationsJS/cleanCode.js
+++
@@ -1,4 +0,0 @@
-// Pas commencer les variables par un verbe, plutôt nom ou adjectif
-// COmmencer les functions par un verbe
-
-//Utiliser des variables en majuscules lorsque ce qu'il se trouve à l'interieur estune CONSTANTE qui ne changera jamais
